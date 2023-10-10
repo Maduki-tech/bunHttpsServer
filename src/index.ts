4ade15bc4bfaf2c2a2bd359bf8ad5b85c0e9758c
Maduki-tech <d.schlueter1011@gmail.com>
2023-10-10 08:12:31 +0200

    basic bun Server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.get("/", () => "Hello World!");
+
+app.get("/hello/:name", ({params: {name}}) => {
+    return `Hello this is from ${name}!`;
+});
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
+
+console.log("App is running on localhost:3000");
