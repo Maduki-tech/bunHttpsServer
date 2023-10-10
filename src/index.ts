8db412e9efe5a17d664cf49990e6487bb9ad0b46
Maduki-tech <d.schlueter1011@gmail.com>
2023-10-10 08:32:25 +0200

    added sqlite

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,55 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { Database } from "bun:sqlite";
 
 const app = new Elysia();
+app.use(cors());
 
-app.get("/", () => "Hello World!");
+const DB = new Database("mydb.sqlite", { create: true });
 
-app.get("/hello/:name", ({params: {name}}) => {
-    return `Hello this is from ${name}!`;
+DB.query(
+  `CREATE TABLE IF NOT EXISTS Messages (
+             id INTEGER PRIMARY KEY AUTOINCREMENT,
+         message Text
+         );`,
+).run();
+
+app.get("/", (context) => {
+  const query = DB.query("SELECT * FROM Messages;");
+  const result = query.all();
+
+  console.log(result);
+  context.set.status = 200;
+
+  return new Response(JSON.stringify({ messages: result }), {
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+});
+
+app.post(
+  "/add",
+  ({ body }) => {
+    const message = body?.message;
+    console.log(message);
+    const query = DB.query("INSERT INTO Messages (message) VALUES (?1);");
+    query.run(message);
+    return new Response(JSON.stringify({ message: "success" }), {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  },
+  {
+    body: t.Object({
+      message: t.String(),
+    }),
+  },
+);
+
+app.get("/hello/:name", ({ params: { name } }) => {
+  return `Hello this is from ${name}!`;
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
